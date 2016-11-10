c54f87b5ecb1adfbb6199cca0098d340b9e336e2
lakerko <martin.ockovsky@gmail.com>
2016-11-11 00:28:03 +0100

    Selecting user now shows communication only with him. Refactoring

--- src/app/mail/mail.component.ts
+++ src/app/mail/mail.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit }	from '@angular/core';
-import { Router }				from '@angular/router';
+import { Router }				      from '@angular/router';
 
-import { MailService }			from './mail.service';
-import { UserService }			from '../services/user.service';
+import { MailService }				from './mail.service';
+import { UserService }				from '../services/user.service';
+
+import {KeysPipe} 						from '../filters/keys.pipe';
 
 
 
@@ -15,10 +17,9 @@ export class MailComponent implements OnInit {
 
 	mailTo = '';
 	mailText = '';
-	mails = [];
-	mailUsers = [];
-	user;
-	clicked = false;
+	mailUsers = {};
+	user = null;
+	selectedUser = false;
 
 	constructor(
 		private mailService: MailService,
@@ -49,33 +50,22 @@ export class MailComponent implements OnInit {
 	}
 
 	onGotMails = (json) => {
-		this.mails = json.mails;
+		const mails = json.mails;
 
-		if (!this.mails.length)
+		if (!mails.length)
 			return;
 
-		this.mailTo = this.mails[0].mail_to_name;
+		this.mailTo = mails[0].mail_to_name;
 
-		const mailsLength = this.mails.length;
-		let tempUsersArr = [];
-		for (let i = 0; i < mailsLength; i++) {
-			if (this.user.userId == this.mails[i].mail_from) continue;
-			if (!tempUsersArr[this.mails[i].mail_from]) {
-				tempUsersArr[this.mails[i].mail_from] = [];
-			}
-			tempUsersArr[this.mails[i].mail_from].push(this.mails[i]);
-		}
-		tempUsersArr = tempUsersArr.map(user => {
-			return {
-				id: user[0].mail_from,
-				name: user[0].mail_from_name,
-				mails: user,
+		mails.forEach(mail => {
+			if (this.user.userId != mail.mail_from) {
+				if (!this.mailUsers[mail.mail_from]) {
+					this.mailUsers[mail.mail_from] = [];
+				}
+				this.mailUsers[mail.mail_from].push(mail);
 			}
 		});
-
-		tempUsersArr.forEach(user => {
-			this.mailUsers.push(user);
-		});
+		this.mailUsers = this.mapToArr(this.mailUsers);
 	}
 
 	onMailSent = (json) => {
@@ -84,7 +74,15 @@ export class MailComponent implements OnInit {
 		this.getMails();
 	}
 
-	userSelected = (user) => {
-		this.clicked = true;
+	selectUser = (user) => {
+		this.selectedUser = user;
+	}
+
+	mapToArr = (map) => {
+		let arr = [];
+		for (var key in map) {
+			arr.push( {key: key, value: map[key]} );
+		}
+		return arr;
 	}
 }
