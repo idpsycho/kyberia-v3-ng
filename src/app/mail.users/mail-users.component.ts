f69812d2da6eef9936bd787153ac08c17f18801f
Unknown <mayo@cvernovka.com>
2016-12-04 23:53:54 +0100

    styled: user list

--- src/app/mail.users/mail-users.component.ts
+++ src/app/mail.users/mail-users.component.ts
@@ -3,6 +3,7 @@ import {Component, Input} from '@angular/core';
 @Component({
 	selector: 'mail-users',
 	templateUrl: './mail-users.component.html',
+	styleUrls: ['mail-users.component.scss'],
 })
 export class MailUsersComponent {
 	@Input() mapUsers;
