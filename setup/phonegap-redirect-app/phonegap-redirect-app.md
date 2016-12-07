347417ff4850b746ceadcd1f179b877c81435116
psycho <id.psycho@gmail.com>
2016-12-07 17:41:51 +0100

    phonegap appka ktora redirectne na v3ng.brm.sk

---
+++ setup/phonegap-redirect-app/phonegap-redirect-app.md
@@ -0,0 +1,15 @@
+# how to
+1. zbalit ako:
+	www.ZIP
+
+2. upload to:
+	https://build.phonegap.com/apps/2348841/builds
+
+3. download:
+	kyberia.ipa
+
+4. upload to:
+	http://diawi.com/
+
+5. open link on iphone device to install
+
---
+++ setup/phonegap-redirect-app/www/js/index.js
@@ -0,0 +1,13 @@
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+
+        window.location = 'http://v3ng.brm.sk';
+
+    },
+};
